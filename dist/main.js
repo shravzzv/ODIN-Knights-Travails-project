9567f041b081e35755505e0f9f91448e0ec6bfa6
sai shravan <saishravan384@gmail.com>
2023-11-15 15:07:45 +0530

    feat: Complete assignment

--- dist/main.js
+++ dist/main.js
@@ -7,90 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/classes/graph.js":
-/*!******************************!*\
-  !*** ./src/classes/graph.js ***!
-  \******************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Graph: () => (/* binding */ Graph)\n/* harmony export */ });\nclass Graph {\r\n  constructor() {\r\n    this.adjList = new Map()\r\n  }\r\n\r\n  addVertex(vertex) {\r\n    if (!this.adjList.has(vertex)) {\r\n      this.adjList.set(`${vertex}`, [])\r\n    }\r\n  }\r\n\r\n  addEdge(src, dest) {\r\n    if (!this.adjList.has(src) || !this.adjList.has(dest)) {\r\n      // console.error('Invalid vertex')\r\n      return\r\n    }\r\n\r\n    if (!this.adjList.get(src).includes(dest)) {\r\n      this.adjList.get(src).push(dest)\r\n    } else {\r\n      // console.error('Edge already exists')\r\n    }\r\n\r\n    // since the graph is undirected, add an edge from dest to src also\r\n    if (!this.adjList.get(dest).includes(src)) {\r\n      this.adjList.get(dest).push(src)\r\n    } else {\r\n      // console.error('Edge already exists')\r\n    }\r\n  }\r\n\r\n  doBFS(startingVertex, fn = null) {\r\n    if (!this.adjList.has(startingVertex)) {\r\n      console.error('Invalid starting vertex')\r\n      return\r\n    }\r\n\r\n    const visited = {}\r\n    visited[startingVertex] = true\r\n    const queue = []\r\n    queue.push(startingVertex)\r\n\r\n    while (queue.length > 0) {\r\n      let vertex = queue.shift()\r\n      fn ? fn(vertex) : console.log(vertex)\r\n\r\n      const adjacents = this.adjList.get(vertex)\r\n\r\n      for (let adjacent of adjacents) {\r\n        if (!visited[adjacent]) {\r\n          visited[adjacent] = true\r\n          queue.push(adjacent)\r\n        }\r\n      }\r\n    }\r\n  }\r\n\r\n  print() {\r\n    console.log(this.adjList)\r\n  }\r\n\r\n  knightMoves(start, end) {\r\n    console.log(start + ' ' + end)\r\n  }\r\n}\r\n\n\n//# sourceURL=webpack://odin-knights-travails-project/./src/classes/graph.js?");
-
-/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _classes_graph__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./classes/graph */ \"./src/classes/graph.js\");\n\r\n\r\nconst gameBoard = new _classes_graph__WEBPACK_IMPORTED_MODULE_0__.Graph()\r\n\r\n// add all vertices\r\nfor (let i = 0; i < 8; i++) {\r\n  for (let j = 0; j < 8; j++) {\r\n    let key = [i, j]\r\n    gameBoard.addVertex(key)\r\n  }\r\n}\r\n\r\nconst knightChoices = [\r\n  [2, 1],\r\n  [1, 2],\r\n  [-1, 2],\r\n  [-2, 1],\r\n  [-2, -1],\r\n  [-1, -2],\r\n  [1, -2],\r\n  [2, -1],\r\n]\r\n\r\n// add all edges\r\nfor (const key of gameBoard.adjList.keys()) {\r\n  for (const move of knightChoices) {\r\n    let destX = parseInt(key[0]) + parseInt(move[0])\r\n    let destY = parseInt(key[2]) + parseInt(move[1])\r\n\r\n    if (destX >= 0 && destX < 8 && destY >= 0 && destY < 8) {\r\n      let destination = `${destX},${destY}`\r\n      gameBoard.addEdge(key, destination)\r\n    }\r\n  }\r\n}\r\n\r\ngameBoard.print()\r\ngameBoard.doBFS('5,5')\r\n\r\ngameBoard.knightMoves('1,1', '5,4')\r\n\n\n//# sourceURL=webpack://odin-knights-travails-project/./src/index.js?");
+eval("const game = (function () {\r\n  const _adjList = new Map()\r\n\r\n  const _knightChoices = [\r\n    [2, 1],\r\n    [1, 2],\r\n    [-1, 2],\r\n    [-2, 1],\r\n    [-2, -1],\r\n    [-1, -2],\r\n    [1, -2],\r\n    [2, -1],\r\n  ]\r\n\r\n  const _addVertex = (vertex) => {\r\n    if (!_adjList.has(vertex)) {\r\n      _adjList.set(vertex, [])\r\n    }\r\n  }\r\n\r\n  for (let i = 0; i < 8; i++) {\r\n    for (let j = 0; j < 8; j++) {\r\n      // convert arrays into strings before adding to the list\r\n      _addVertex([i, j].join(','))\r\n    }\r\n  }\r\n\r\n  const _addEdge = (start, target) => {\r\n    if (_adjList.has(start) && _adjList.has(target)) {\r\n      if (!_adjList.get(start).includes(target)) {\r\n        _adjList.get(start).push(target)\r\n      }\r\n      if (!_adjList.get(target).includes(start)) {\r\n        _adjList.get(target).push(start)\r\n      }\r\n    }\r\n  }\r\n\r\n  for (const key of _adjList.keys()) {\r\n    // key is a string as '1,1'\r\n    for (const move of _knightChoices) {\r\n      let destX = parseInt(key[0]) + move[0]\r\n      let destY = parseInt(key[2]) + move[1]\r\n\r\n      if (destX >= 0 && destX < 8 && destY >= 0 && destY < 8) {\r\n        _addEdge(key, [destX, destY].join(','))\r\n      }\r\n    }\r\n  }\r\n\r\n  return {\r\n    print: () => console.log(_adjList),\r\n    knightMoves: (start, end, graph = _adjList) => {\r\n      // return the shortest path between start and end vertices using bfs\r\n\r\n      start = start.join(',')\r\n      end = end.join(',')\r\n      if (!graph.has(start) || !graph.has(end)) return 'Invalid start or end'\r\n\r\n      const queue = [[start]]\r\n      const visited = new Set()\r\n\r\n      while (queue.length) {\r\n        const path = queue.shift()\r\n        const vertex = path[path.length - 1]\r\n\r\n        if (!visited.has(vertex)) {\r\n          visited.add(vertex)\r\n\r\n          if (vertex === end) {\r\n            console.log(\r\n              `You made it in ${path.length - 1} moves! Here's your path:`\r\n            )\r\n            return path.map((coord) => coord.split(',').map(Number))\r\n          }\r\n\r\n          for (const neighbor of graph.get(vertex)) {\r\n            const newPath = [...path, neighbor]\r\n            queue.push(newPath)\r\n          }\r\n        }\r\n      }\r\n\r\n      return null // no path found\r\n    },\r\n  }\r\n})()\r\n\r\nconsole.log(game.knightMoves([0, 0], [7, 6]))\r\n\n\n//# sourceURL=webpack://odin-knights-travails-project/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,42 +1,89 @@
-import { Graph } from './classes/graph'
+const game = (function () {
+  const _adjList = new Map()
 
-const gameBoard = new Graph()
+  const _knightChoices = [
+    [2, 1],
+    [1, 2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1],
+  ]
 
-// add all vertices
-for (let i = 0; i < 8; i++) {
-  for (let j = 0; j < 8; j++) {
-    let key = [i, j]
-    gameBoard.addVertex(key)
-  }
-}
-
-const knightChoices = [
-  [2, 1],
-  [1, 2],
-  [-1, 2],
-  [-2, 1],
-  [-2, -1],
-  [-1, -2],
-  [1, -2],
-  [2, -1],
-]
-
-// add all edges
-for (const key of gameBoard.adjList.keys()) {
-  for (const move of knightChoices) {
-    let destX = parseInt(key[0]) + parseInt(move[0])
-    let destY = parseInt(key[2]) + parseInt(move[1])
-
-    if (destX >= 0 && destX < 8 && destY >= 0 && destY < 8) {
-      let destination = `${destX},${destY}`
-      gameBoard.addEdge(key, destination)
+  const _addVertex = (vertex) => {
+    if (!_adjList.has(vertex)) {
+      _adjList.set(vertex, [])
     }
   }
-}
 
-gameBoard.print()
-gameBoard.doBFS('5,5')
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      // convert arrays into strings before adding to the list
+      _addVertex([i, j].join(','))
+    }
+  }
 
-gameBoard.knightMoves('1,1', '5,4')
+  const _addEdge = (start, target) => {
+    if (_adjList.has(start) && _adjList.has(target)) {
+      if (!_adjList.get(start).includes(target)) {
+        _adjList.get(start).push(target)
+      }
+      if (!_adjList.get(target).includes(start)) {
+        _adjList.get(target).push(start)
+      }
+    }
+  }
 
-// todo: Implement knightMoves function
+  for (const key of _adjList.keys()) {
+    // key is a string as '1,1'
+    for (const move of _knightChoices) {
+      let destX = parseInt(key[0]) + move[0]
+      let destY = parseInt(key[2]) + move[1]
+
+      if (destX >= 0 && destX < 8 && destY >= 0 && destY < 8) {
+        _addEdge(key, [destX, destY].join(','))
+      }
+    }
+  }
+
+  return {
+    print: () => console.log(_adjList),
+    knightMoves: (start, end, graph = _adjList) => {
+      // return the shortest path between start and end vertices using bfs
+
+      start = start.join(',')
+      end = end.join(',')
+      if (!graph.has(start) || !graph.has(end)) return 'Invalid start or end'
+
+      const queue = [[start]]
+      const visited = new Set()
+
+      while (queue.length) {
+        const path = queue.shift()
+        const vertex = path[path.length - 1]
+
+        if (!visited.has(vertex)) {
+          visited.add(vertex)
+
+          if (vertex === end) {
+            console.log(
+              `You made it in ${path.length - 1} moves! Here's your path:`
+            )
+            return path.map((coord) => coord.split(',').map(Number))
+          }
+
+          for (const neighbor of graph.get(vertex)) {
+            const newPath = [...path, neighbor]
+            queue.push(newPath)
+          }
+        }
+      }
+
+      return null // no path found
+    },
+  }
+})()
+
+console.log(game.knightMoves([0, 0], [7, 6]))
